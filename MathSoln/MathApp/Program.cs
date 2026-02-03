46396667ac02400ecd8dd46f5931977375bfee56
Nilesh Ghule <nilesh@sunbeaminfo.com>
2026-02-03 13:26:50 +0530

    Class Lib & Soln

---
+++ MathSoln/MathApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MathLib;
+
+namespace MathApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMath obj = new MyMath();
+            int res1 = obj.Add(22, 7);
+            Console.WriteLine("Add Result: " + res1);
+            int res2 = obj.Subtract(22, 7);
+            Console.WriteLine("Subtract Result: {0}", res2);
+        }
+    }
+}
---
+++ MathSoln/MathLib/MyMath.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLib
+{
+    public class MyMath
+    {
+        public int Add(int x, int y)
+        {
+            return x + y; 
+        }
+        public int Subtract(int x, int y) {
+            return x - y; 
+        }
+    }
+}
